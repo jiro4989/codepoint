9df6d27a170b52e2359c23345138bc54bc838fe2
jiro4989 <jiroron666@gmail.com>
2020-07-05 01:45:01 +0000

    update version

--- src/codepoint.nim
+++ src/codepoint.nim
@@ -2,9 +2,9 @@ import unicode, strutils
 
 const
   version = """codepoint version 1.0.0
-Copyright (c) 2019 jiro4989
+Copyright (c) 2020 jiro4989
 Released under the MIT License.
-https://github.com/jiro4989/clitools"""
+https://github.com/jiro4989/codepoint"""
 
 iterator genCpRow(line: string, delimiter: string): string =
   for ch in line.toRunes:
